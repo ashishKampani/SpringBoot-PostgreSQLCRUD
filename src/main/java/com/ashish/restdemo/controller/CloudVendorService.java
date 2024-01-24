8f1f5508ad41355ec89612057598b1ef82d3f2cf
ashishkamapni8 <ashishkampani8@gmail.com>
2024-01-24 21:42:01 +0530

    Initial commit

---
+++ src/main/java/com/ashish/restdemo/controller/CloudVendorService.java
@@ -0,0 +1,41 @@
+package com.ashish.restdemo.controller;
+
+import com.ashish.restdemo.model.CloudVendor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ashish")
+public class CloudVendorService {
+
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getVendorDetails(String vendorId){
+        return cloudVendor;
+               /* new CloudVendor("1","ashish",
+                "b-3-30","90909090XX");*/
+    }
+
+    @PostMapping
+    public String createVendor(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Vendor added successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendor(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "cloud vendor UPDATED Successfully ";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteVendor(String vendorId){
+        this.cloudVendor = null;
+        return "Vendor DELETED Successfully";
+    }
+}
---
+++ src/main/java/com/ashish/restdemo/model/CloudVendor.java
@@ -0,0 +1,53 @@
+package com.ashish.restdemo.model;
+
+public class CloudVendor {
+
+    private String VendorId;
+
+    private String VendorName;
+
+    private String VendorAddress;
+
+    private String VendorPhoneNumber;
+
+    public CloudVendor() { }
+
+    public CloudVendor(String vendorId, String vendorName, String vendorAddress, String vendorPhoneNumber) {
+        VendorId = vendorId;
+        VendorName = vendorName;
+        VendorAddress = vendorAddress;
+        VendorPhoneNumber = vendorPhoneNumber;
+    }
+
+    public String getVendorId() {
+        return VendorId;
+    }
+
+    public void setVendorId(String vendorId) {
+        VendorId = vendorId;
+    }
+
+    public String getVendorName() {
+        return VendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        VendorName = vendorName;
+    }
+
+    public String getVendorAddress() {
+        return VendorAddress;
+    }
+
+    public void setVendorAddress(String vendorAddress) {
+        VendorAddress = vendorAddress;
+    }
+
+    public String getVendorPhoneNumber() {
+        return VendorPhoneNumber;
+    }
+
+    public void setVendorPhoneNumber(String vendorPhoneNumber) {
+        VendorPhoneNumber = vendorPhoneNumber;
+    }
+}
